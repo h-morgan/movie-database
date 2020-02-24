36101f70e394cb3152048645aabdbcec66ab3b72
Haley Morgan <haley.morgan@bostonfusion.com>
2020-02-23 21:46:40 -0500

    finish hw

--- src/hw3/Tests.java
+++ src/hw3/Tests.java
@@ -9,23 +9,11 @@ public class Tests {
 	public static void main(String[] args) {
 		
 		//pass the path to the file as a parameter
-		try {
-			File file = new File("movies.txt");
-			Scanner sc = new Scanner(file);
+		String s1 = "qbc";
+		String s2 = "qbc";
 		
-			int h = 0;
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-				h++;
-				if (h > 20) {
-					break;
-				}	
-			}
-			sc.close();
-		
-		} catch (Exception e){
-			System.out.println("File not found");
-		}
+		boolean eq = (s1 == s2);
+		System.out.println(s1.equals(s2));
 
 	}
 	}
